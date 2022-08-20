44fa060fc51c7baac3efe7283f1cafdff0aa97fa
Vasil Valchev <96626766+VsVch@users.noreply.github.com>
2022-08-20 23:51:05 +0300

    Add project files.

---
+++ 03. Generic Scale/EqualityScale.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericScale
+{
+    
+    public class EqualityScale<T>
+    {
+        //Fieald
+        private T firstElement;
+        private T secondElement;
+
+        //Constructor
+        public EqualityScale(T first, T second)
+        {
+            this.firstElement = first;
+            this.secondElement = second;
+        }
+
+        /// <summary>
+        /// The method should return true if the elements are eauql.
+        /// </summary>
+        /// <returns></returns>
+        public bool AreEqual()
+        {
+            return this.firstElement.Equals(this.secondElement);
+        }
+    }
+    
+}
